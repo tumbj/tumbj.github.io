28ee095ab327008539e4941bad4de849771918a5
tumbj <thanadol.ko@ku.th>
2019-02-10 05:56:24 +0700

    update advance search

--- jquery3/myscript.js
+++ jquery3/myscript.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
-  
     $('#submit-btn').click(function() {
         $("tbody").html("");
         $("#result").text("");
         console.log("first");
-        var output;
+        
         $.getJSON("data.json",function(data){
             var input = $("#input-text").val();
             var regex = new RegExp(input, 'i');
-           
+            var output;
             $.each(data, function(key, val){
                 if((val.nameTH.search(regex)!= -1) || (val.nameENG.search(regex)!= -1) 
                     || (val.province.search(regex)!= -1) || (val.height.search(regex)!= -1)){
@@ -18,16 +17,18 @@ $(document).ready(function() {
                     output += "<td id='"+key+"'>"+val.nameENG+"</td>";        
                     output += "<td id='"+key+"'>"+val.province+"</td>";
                     output += "<td id='"+key+"'>"+val.height+"</td>";    
-                    output += "</tr>";
+                    output += "</tr>";      
+                    // $("#result").text("");   
                     }   
-                else{
-                    $("#result").text("Search not founded.");
-                }
+                
             });
+            if(output==null){
+                    $("#result").text("Search not founded.");
+            }
      
+            $("#result").val("");   
         $("#input-text").val("");
-           
-            $("tbody").html(output);
+        $("tbody").html(output);
 
 
         });
@@ -36,22 +37,36 @@ $(document).ready(function() {
 
     });
 
-   
+    $("#adv-btn").click(function(){
+        var slide1 = $("#sli1").val();
+        var slide2 = $("#sli2").val();
+        $("#out1").text(slide1+"min");
+        $("#out2").text(slide2+"mam");
+        console.log("adv");
+
+    });
+
 
     $.ajax({
         url: "data.json",
         dataType: "json"
     }).done(function(response) {
-        console.log(response);
+        console.log(response);     
+        var out;
         response.forEach(element => {
-            console.log(element.name, element.age);
-            output += "<tr>";
-            output += "<td>"+val.nameTH+"</td>";
-            output += "<td>"+val.nameENG+"</td>";        
-            output += "<td>"+val.province+"</td>";
-            output += "<td>"+val.height+"</td>";    
-            output += "</tr>";
-        });
+           
+            out += "<tr>";
+            out += "<td>"+element.nameTH+"</td>";
+            out += "<td>"+element.nameENG+"</td>";        
+            out += "<td>"+element.province+"</td>";
+            out += "<td>"+element.height+"</td>";    
+            out += "</tr>";
+           
+        })
+        $("tbody").html(out);
+        $("#result").text("");
+      
         
     });
 });
+
